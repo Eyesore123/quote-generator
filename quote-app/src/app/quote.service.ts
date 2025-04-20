159394630e4c3c62fca8d36779d76193f224728c
Eyesore123 <joni.putkinen@protonmail.com>
2025-04-20 21:41:48 +0300

    updates

--- quote-app/src/app/quote.service.ts
+++ quote-app/src/app/quote.service.ts
@@ -2,25 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface Quote {
-    id: number;
-    author: string;
-    text: string;
-}
-
 @Injectable({
-    providedIn: 'root'
-  })
-  export class QuoteService {
-    private apiUrl = 'http://localhost:5000/'; // Or whatever your backend URL is
-  
-    constructor(private http: HttpClient) {}
-  
-    getAllQuotes(): Observable<Quote[]> {
-      return this.http.get<Quote[]>(this.apiUrl);
-    }
-  
-    getRandomQuote(): Observable<Quote> {
-      return this.http.get<Quote>(`${this.apiUrl}/random`);
-    }
+  providedIn: 'root'
+})
+export class QuoteService {
+  private apiUrl = 'http://127.0.0.1:5000';
+
+  constructor(private http: HttpClient) {}
+
+  getQuoteByCategory(category: string): Observable<{ quote: string }> {
+    return this.http.get<{ quote: string }>(`${this.apiUrl}/quote/${category}`);
   }
+}
--- quote-backend/app.py
+++ quote-backend/app.py
@@ -4,7 +4,7 @@ import random
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, origins="*")
 
 quotes = {
     "philosophy": [
