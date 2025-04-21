86b156f752068408af184f59470252f10c7cf62a
Eyesore123 <joni.putkinen@protonmail.com>
2025-04-21 09:44:49 +0300

    updates

--- quote-app/src/app/app.routes.ts
+++ quote-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@
 import { Routes } from '@angular/router';
 import { QuoteListComponent } from './quote-list/quote-list.component';
 import { RandomQuoteComponent } from './random-quote/random-quote.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/quotes', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'quotes', component: QuoteListComponent },
-  { path: 'random-quote', component: RandomQuoteComponent }
+  { path: 'random-quote', component: RandomQuoteComponent },
+  { path: '**', redirectTo: '' },
 ];
