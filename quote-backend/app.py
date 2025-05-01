68d9e038571b86a92e7be51f6057bf13e4a33017
Eyesore123 <joni.putkinen@protonmail.com>
2025-05-01 17:03:38 +0300

    -

--- quote-backend/app.py
+++ quote-backend/app.py
@@ -83,17 +83,17 @@ def get_all_quotes():
             all_quotes.append({**q, "category": category})
     return jsonify(all_quotes)
 
-# === Serve Angular app for all frontend routes ===
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    file_path = os.path.join(app.static_folder, path)
+# # === Serve Angular app for all frontend routes ===
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     file_path = os.path.join(app.static_folder, path)
 
-    if path != "" and os.path.exists(file_path) and os.path.isfile(file_path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        # Serve index.html for Angular routes like /unsubscribed, /search, etc.
-        return send_from_directory(app.static_folder, 'index.html')
+#     if path != "" and os.path.exists(file_path) and os.path.isfile(file_path):
+#         return send_from_directory(app.static_folder, path)
+#     else:
+#         # Serve index.html for Angular routes like /unsubscribed, /search, etc.
+#         return send_from_directory(app.static_folder, 'index.html')
 
 # === Start scheduler ===
 start_scheduler(app)
